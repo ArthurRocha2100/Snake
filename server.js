72d60371925bfc122d96a8084e841153e65e8bee
UniforArthur <arthur.moraes@edu.unifor.br>
2023-05-23 19:20:36 -0300

    Organizando esturtúra do projeto

--- server.js
+++
@@ -1,6 +0,0 @@
-import app from "./src/js/app.js"
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    console.log(`Servidor escultando em http://localhost:${port}`);
-})
--- src/config/dbConect.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-
-
-mongoose.connect("mongodb+srv://administrador:admin1234S@atividadefinaldpw.lv59khc.mongodb.net/AtividadeFinalDPW");
-
-let db = mongoose.connection;
-
-export default db;
